95e5bdb2354de9b207cf8148aaa941a2c83765ec
Kruy Vanna <kruyvanna.io@gmail.com>
2017-07-07 11:32:07 +0700

    flat state data

--- src/createReducerAsync.js
+++ src/createReducerAsync.js
@@ -1,30 +1,33 @@
-import {createReducer} from 'redux-act';
+import { createReducer } from 'redux-act'
 
 const defaultsState = {
-    loading: false,
-    request: null,
-    data: null,
-    error: null
-};
+  loading: false,
+  request: null,
+  data: null,
+  error: null
+}
 
 export default function createReducerAsync(actionAsync) {
-    return createReducer({
-        [actionAsync.request]: (state, payload) => ({
-            ...state,
-            request: payload,
-            loading: true,
-            error: null
-        }),
-        [actionAsync.ok]: (state, payload) => ({
-            ...state,
-            loading: false,
-            data: payload.response
-        }),
-        [actionAsync.error]: (state, payload) => ({
-            ...state,
-            loading: false,
-            error: payload.error
-        }),
-        [actionAsync.reset]: () => (defaultsState)
-    }, defaultsState);
+  return createReducer(
+    {
+      [actionAsync.request]: (state, payload) => ({
+        ...state,
+        request: payload,
+        loading: true,
+        error: null
+      }),
+      [actionAsync.ok]: (state, payload) => ({
+        ...state,
+        loading: false,
+        data: payload.response.data
+      }),
+      [actionAsync.error]: (state, payload) => ({
+        ...state,
+        loading: false,
+        error: payload.error
+      }),
+      [actionAsync.reset]: () => defaultsState
+    },
+    defaultsState
+  )
 }
